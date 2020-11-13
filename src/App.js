e32aeb9ea979063110bb04cf0805feeee6542fd0
Abiodun Sulaiman <hello@abiodun.dev>
2020-11-13 16:55:40 +0100

    Added favourite and filter functionality

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ function App() {
   const [input, setInput] = useState({
     name: "",
     phoneNumber: "", 
-    address: ""
+    address: "",
+    favourite: false
   })
   const [contactToEdit, setContactToEdit] = useState(null)
   const [contacts, setContacts] = useState([])
+  const [filteredContacts, setFilteredContacts] = useState([])
+  const [showActive, setShowActive] = useState("ALL")
 
   useEffect(() => {
     //fetch from ls
     const initialContacts = localStorage.getItem("phonebook")
-    setContacts(JSON.parse(initialContacts))
-    
+    const parsedContacts = JSON.parse(initialContacts)
+    setContacts(parsedContacts)
+    setFilteredContacts(parsedContacts)
   }, [])
+
 
   function handleChange(e) {
     const {name, value} = e.target
@@ -27,12 +32,13 @@ function App() {
       name: input.name,
       phoneNumber: input.phoneNumber,
       id: input.id || new Date().getTime(),
-      address: input.address
+      address: input.address,
+      favourite: input.id ? input.favourite : false
     }
     const iterableContacts = contacts || []
     let mergedInput = [...iterableContacts,newInput]
     if(input.id) {
-      mergedInput = contacts.map(contact => contact.id === input.id ? 
+      mergedInput = iterableContacts.map(contact => contact.id === input.id ? 
         {...contact, name: input.name, address: input.address, phoneNumber: input.phoneNumber}
          : contact
       )
@@ -43,7 +49,8 @@ function App() {
     setInput({
       name: "",
       phoneNumber: "", 
-      address: ""
+      address: "",
+      favourite: false
     })
     setContactToEdit(null)
   }
@@ -51,13 +58,39 @@ function App() {
     // const filteredContacts = contacts.filter(function(contact) {
     //   return contact.id !== contactId
     // })
-    const filteredContacts = contacts.filter((contact) => contact.id !== contactId)
-    setContacts(filteredContacts)
-    localStorage.setItem("phonebook", JSON.stringify(filteredContacts))
+    const remainingContacts = contacts.filter((contact) => contact.id !== contactId)
+    setContacts(remainingContacts)
+    localStorage.setItem("phonebook", JSON.stringify(remainingContacts))
   }
   function handleEdit(contact) {
     setContactToEdit(contact)
     setInput(contact)
+  }
+  function toggleFavourite(contactIdToToggle) {
+    const iterableContacts = contacts || []
+    const mergedInput = iterableContacts.map(contact => contact.id === contactIdToToggle ? 
+      {...contact, favourite: !contact.favourite}
+       : contact
+    )
+    //update db
+    localStorage.setItem("phonebook", JSON.stringify(mergedInput))
+    //update state
+    setContacts(mergedInput)
+  }
+  function toggleFilter() {
+    let currentState;
+    if(showActive === "ALL") {
+      currentState = "FAVOURITE"
+      const iterableContacts = contacts || []
+      const favContacts = iterableContacts.filter(iterableContact => (
+        iterableContact.favourite === true
+      ))
+      setFilteredContacts(favContacts)
+    } else {
+      currentState = "ALL"
+      setFilteredContacts(contacts)
+    }
+    setShowActive(currentState)
   }
   return (
     <div className="container">
@@ -80,8 +113,16 @@ function App() {
           <button className="contact-form-submit">{contactToEdit ? "Update" : "Create"} Contact</button>
         </form>
         <div className="contact-body">
+        <div className="filter-container">
+          <button
+          onClick = {toggleFilter}
+           className = {`${showActive === "ALL" ? "is-active" : ""}`} disabled = {showActive === "ALL"}>Show all</button>
+          <button
+          onClick = {toggleFilter}
+           className={`${showActive === "FAVOURITE" ? "is-active" : ""}`} disabled = {showActive === "FAVOURITE"}>Show favourite</button>
+        </div>
         {
-          (contacts && contacts.length > 0) ? (contacts.map(contact => (
+          (filteredContacts && filteredContacts.length > 0) ? (filteredContacts.map(contact => (
             <div className="contact" key={contact.id}>
               <div className="contact-details">
                 <div className="contact-name">{contact.name}</div>
@@ -89,6 +130,17 @@ function App() {
                 <div className="contact-phone">{contact.address}</div>
               </div>
               <div className="contact-action">
+              {
+                contact.favourite ? (
+                <button onClick = {() => toggleFavourite(contact.id)} title="Mark as non-favourite">
+                ⭐️
+                </button>
+                ) : (
+                <button onClick = {() => toggleFavourite(contact.id)} title="Mark as favourite">
+                ✩
+                </button>
+                )
+              }
                 <button onClick = {() => handleEdit(contact)}>Edit</button>
                 <button onClick = {() => handleDelete(contact.id)}>Delete</button>
               </div>
